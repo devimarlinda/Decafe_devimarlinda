374e37bda8a942eac24635b342d872c083fc46f8
devimarlinda <126534013+devimarlinda@users.noreply.github.com>
2023-10-29 11:11:51 +0700

    Update connect.php

--- proses/connect.php
+++ proses/connect.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn = mysqli_connect("localhost","root","","db_decafe");
+    $conn = mysqli_connect("localhost","u837952942_devi","4IPOw[q>ys;U","u837952942_devi");
     if(!$conn){
         echo "Gagal koneksi";
     }
